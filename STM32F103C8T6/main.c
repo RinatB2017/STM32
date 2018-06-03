24a031c8993c722950792a6dbc07b19e7e089eaa
RinatB2017 <tux4096@gmail.com>
2018-06-03 22:33:38 +0300

    update

--- STM32F103C8T6/main.c
+++ STM32F103C8T6/main.c
@@ -20,30 +20,30 @@ void Delay_ms(uint32_t ms)
 
 int main(void)
 {
-//	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-//
-//	GPIO_InitTypeDef GPIO_InitStructure;
-//	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
-//	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-//	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-//	GPIO_Init(GPIOB, &GPIO_InitStructure);
+#if 1
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
 
-//    while(1)
-//    {
-//    	GPIO_SetBits(GPIOB,	GPIO_Pin_12);
-//    	Delay_ms(500);
-//    	GPIO_ResetBits(GPIOB,	GPIO_Pin_12);
-//    	Delay_ms(500);
-//    }
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
 
+    while(1)
+    {
+    	GPIO_SetBits(GPIOB,	GPIO_Pin_12);
+    	Delay_ms(500);
+    	GPIO_ResetBits(GPIOB,	GPIO_Pin_12);
+    	Delay_ms(500);
+    }
+#else
     ws2812b_Init();
 
     while (1)
     {
         while (!ws2812b_IsReady()); // wait
-
         // Заполнить RGB-буффер
-
         ws2812b_SendRGB(leds, NUM_LEDS);
     }
+#endif
 }
