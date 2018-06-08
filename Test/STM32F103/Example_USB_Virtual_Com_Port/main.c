91373d82526d163691bb0d1b0abb87a9d641aa94
RinatB2017 <tux4096@gmail.com>
2018-06-08 16:57:48 +0300

    update

--- Test/STM32F103/Example_USB_Virtual_Com_Port/main.c
+++ Test/STM32F103/Example_USB_Virtual_Com_Port/main.c
@@ -14,107 +14,107 @@ uint32_t packet_receive=1;
 void SetSysClockTo72(void)
 {
 	ErrorStatus HSEStartUpStatus;
-    /* SYSCLK, HCLK, PCLK2 and PCLK1 configuration -----------------------------*/
-    /* RCC system reset(for debug purpose) */
-    RCC_DeInit();
+	/* SYSCLK, HCLK, PCLK2 and PCLK1 configuration -----------------------------*/
+	/* RCC system reset(for debug purpose) */
+	RCC_DeInit();
 
-    /* Enable HSE */
-    RCC_HSEConfig( RCC_HSE_ON);
+	/* Enable HSE */
+	RCC_HSEConfig( RCC_HSE_ON);
 
-    /* Wait till HSE is ready */
-    HSEStartUpStatus = RCC_WaitForHSEStartUp();
+	/* Wait till HSE is ready */
+	HSEStartUpStatus = RCC_WaitForHSEStartUp();
 
-    if (HSEStartUpStatus == SUCCESS)
-    {
-        /* Enable Prefetch Buffer */
-    	//FLASH_PrefetchBufferCmd( FLASH_PrefetchBuffer_Enable);
+	if (HSEStartUpStatus == SUCCESS)
+	{
+		/* Enable Prefetch Buffer */
+		//FLASH_PrefetchBufferCmd( FLASH_PrefetchBuffer_Enable);
 
-        /* Flash 2 wait state */
-        //FLASH_SetLatency( FLASH_Latency_2);
+		/* Flash 2 wait state */
+		//FLASH_SetLatency( FLASH_Latency_2);
 
-        /* HCLK = SYSCLK */
-        RCC_HCLKConfig( RCC_SYSCLK_Div1);
+		/* HCLK = SYSCLK */
+		RCC_HCLKConfig( RCC_SYSCLK_Div1);
 
-        /* PCLK2 = HCLK */
-        RCC_PCLK2Config( RCC_HCLK_Div1);
+		/* PCLK2 = HCLK */
+		RCC_PCLK2Config( RCC_HCLK_Div1);
 
-        /* PCLK1 = HCLK/2 */
-        RCC_PCLK1Config( RCC_HCLK_Div2);
+		/* PCLK1 = HCLK/2 */
+		RCC_PCLK1Config( RCC_HCLK_Div2);
 
-        /* PLLCLK = 8MHz * 9 = 72 MHz */
-        RCC_PLLConfig(0x00010000, RCC_PLLMul_9);
+		/* PLLCLK = 8MHz * 9 = 72 MHz */
+		RCC_PLLConfig(0x00010000, RCC_PLLMul_9);
 
-        /* Enable PLL */
-        RCC_PLLCmd( ENABLE);
+		/* Enable PLL */
+		RCC_PLLCmd( ENABLE);
 
-        /* Wait till PLL is ready */
-        while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
-        {
-        }
+		/* Wait till PLL is ready */
+		while (RCC_GetFlagStatus(RCC_FLAG_PLLRDY) == RESET)
+		{
+		}
 
-        /* Select PLL as system clock source */
-        RCC_SYSCLKConfig( RCC_SYSCLKSource_PLLCLK);
+		/* Select PLL as system clock source */
+		RCC_SYSCLKConfig( RCC_SYSCLKSource_PLLCLK);
 
-        /* Wait till PLL is used as system clock source */
-        while (RCC_GetSYSCLKSource() != 0x08)
-        {
-        }
-    }
-    else
-    { /* If HSE fails to start-up, the application will have wrong clock configuration.
+		/* Wait till PLL is used as system clock source */
+		while (RCC_GetSYSCLKSource() != 0x08)
+		{
+		}
+	}
+	else
+	{ /* If HSE fails to start-up, the application will have wrong clock configuration.
      User can add here some code to deal with this error */
 
-        /* Go to infinite loop */
-        while (1)
-        {
-        }
-    }
+		/* Go to infinite loop */
+		while (1)
+		{
+		}
+	}
 }
 
 int main(void)
 {
-  Set_System();
-  SetSysClockTo72();
-  Set_USBClock();
-  USB_Interrupts_Config();
-  USB_Init();
+	Set_System();
+	SetSysClockTo72();
+	Set_USBClock();
+	USB_Interrupts_Config();
+	USB_Init();
 
-  /* Initialize Leds mounted on STM32 board */
-  GPIO_InitTypeDef  GPIO_InitStructure;
-  /* Initialize LED which connected to PC13, Enable the Clock*/
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-  /* Configure the GPIO_LED pin */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
-  GPIO_SetBits(GPIOC, GPIO_Pin_13);
+	/* Initialize Leds mounted on STM32 board */
+	GPIO_InitTypeDef  GPIO_InitStructure;
+	/* Initialize LED which connected to PC12, Enable the Clock*/
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+	/* Configure the GPIO_LED pin */
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);
+	GPIO_SetBits(GPIOB, GPIO_Pin_12);
 
-  while (1)
-  {
-    if (bDeviceState == CONFIGURED)
-    {
-      CDC_Receive_DATA();
-      // Check to see if we have data yet
-      if (Receive_length  != 0)
-      {
-    	  // If received symbol '1' then LED turn on, else LED turn off
-    	  if (Receive_Buffer[0]=='1') {
-    		  GPIO_ResetBits(GPIOC, GPIO_Pin_13);
-    	  }
-    	  else {
-    		  GPIO_SetBits(GPIOC, GPIO_Pin_13);
-    	  }
+	while (1)
+	{
+		if (bDeviceState == CONFIGURED)
+		{
+			CDC_Receive_DATA();
+			// Check to see if we have data yet
+			if (Receive_length  != 0)
+			{
+				// If received symbol '1' then LED turn on, else LED turn off
+				if (Receive_Buffer[0]=='1') {
+					GPIO_ResetBits(GPIOB, GPIO_Pin_12);
+				}
+				else {
+					GPIO_SetBits(GPIOB, GPIO_Pin_12);
+				}
 
-    	  // Echo
-    	  if (packet_sent == 1) {
-    		  CDC_Send_DATA ((uint8_t*)Receive_Buffer,Receive_length);
-    	  }
+				// Echo
+				if (packet_sent == 1) {
+					CDC_Send_DATA ((uint8_t*)Receive_Buffer,Receive_length);
+				}
 
-    	  Receive_length = 0;
-      }
-    }
-  }
+				Receive_length = 0;
+			}
+		}
+	}
 }
 
 
