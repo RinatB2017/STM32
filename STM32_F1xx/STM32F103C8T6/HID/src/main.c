ea2249ad5ae97a6db20f3abfff2a5a975408b15c
RinatB2017 <tux4096@gmail.com>
2018-07-09 16:30:43 +0300

    update

--- STM32_F1xx/STM32F103C8T6/HID/src/main.c
+++ STM32_F1xx/STM32F103C8T6/HID/src/main.c
@@ -1,12 +1,12 @@
 /**
-  ******************************************************************************
-  * @file    main.c
-  * @author  MCD Application Team
-  * @version V4.0.0
-  * @date    09-September-2013
-  * @brief   RHID demo main file
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file    main.c
+ * @author  MCD Application Team
+ * @version V4.0.0
+ * @date    09-September-2013
+ * @brief   RHID demo main file
+ ******************************************************************************
+ */
 
 
 /* Includes ------------------------------------------------------------------*/
@@ -19,57 +19,48 @@ __IO uint8_t PrevXferComplete = 1;
 
 /* Private functions ---------------------------------------------------------*/
 
-void Delay(volatile uint32_t nCount) {
-	for (; nCount != 0; nCount--);
-}
-
 /*******************************************************************************
-* Function Name  : main.
-* Description    : main routine.
-* Input          : None.
-* Output         : None.
-* Return         : ************************************************************/
+ * Function Name  : main.
+ * Description    : main routine.
+ * Input          : None.
+ * Output         : None.
+ * Return         : ************************************************************/
 int main(void)
 {
-  Set_System();
+	Set_System();
+	USB_Interrupts_Config();
+	Set_USBClock();
+	USB_Init();
 
-  USB_Interrupts_Config();
-
-  Set_USBClock();
-
-  USB_Init();
-
-
-  while (1)
-  {
-
-    if (bDeviceState == CONFIGURED)
-    {
-      if (PrevXferComplete)
-      {
-        RHIDCheckState();
-      }
-    }
-  }
+	while (1)
+	{
+		if (bDeviceState == CONFIGURED)
+		{
+			if (PrevXferComplete)
+			{
+				RHIDCheckState();
+			}
+		}
+	}
 }
 
 #ifdef  USE_FULL_ASSERT
 /*******************************************************************************
-* Function Name  : assert_failed
-* Description    : Reports the name of the source file and the source line number
-*                  where the assert_param error has occurred.
-* Input          : - file: pointer to the source file name
-*                  - line: assert_param error line source number
-* Output         : None
-* Return         : None
-*******************************************************************************/
+ * Function Name  : assert_failed
+ * Description    : Reports the name of the source file and the source line number
+ *                  where the assert_param error has occurred.
+ * Input          : - file: pointer to the source file name
+ *                  - line: assert_param error line source number
+ * Output         : None
+ * Return         : None
+ *******************************************************************************/
 void assert_failed(uint8_t* file, uint32_t line)
 {
-  /* User can add his own implementation to report the file name and line number,
+	/* User can add his own implementation to report the file name and line number,
      ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
 
-  /* Infinite loop */
-  while (1)
-  {}
+	/* Infinite loop */
+	while (1)
+	{}
 }
 #endif
